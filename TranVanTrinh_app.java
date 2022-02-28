b015e4e0c01598c1e1410b75d587e2d0a8b74fd4
TranVanTrinh82 <100277091+TranVanTrinh82@users.noreply.github.com>
2022-02-28 16:29:38 +0700

    Add files via upload

---
+++ TranVanTrinh_app.java
@@ -0,0 +1,11 @@
+package Baitap;
+
+public class TranVanTrinh_app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tran Van Trinh hoc Java");
+	}
+	
+
+}
---
+++ ch2_bai5.java
@@ -0,0 +1,16 @@
+package chuong2;
+import java.util.Scanner;
+public class ch2_bai5 {
+	public static void main(String[] args) {
+		 int number, sum = 0;
+	        Scanner scanner = new Scanner(System.in);
+	        while (sum < 100) {
+	            System.out.println("Nhập vào số nguyên bất kỳ: ");
+	            number = scanner.nextInt();
+	            sum += number;  // sum = sum + number;
+	        }
+	         
+	        System.out.println("Tổng các số nguyên vừa nhập = " + sum);
+	    }
+	
+}
